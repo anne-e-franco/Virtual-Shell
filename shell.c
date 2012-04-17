7012831c51c10f7d77d3a990efeecdc207ef82ab
Anne Franco <anne.e.franco@gmail.com>
2012-04-17 19:40:14 -0400

    commands parse correctly

--- shell.c
+++ shell.c
@@ -17,33 +17,43 @@ int main(){
 	char **argpath; //array of (input) commands returned by makeargv.c
 	//workers
 	int i,j,k;
+	int queue[10];
 	char prompt[] = "Virtual Shell > ";
 	//Basic logic: always present the user with a prompt, until they exit the program.
 	printf("%s", prompt); //once at the beginning (before the loop is entered)
 	while (strcmp(fgets(buffer, 120, stdin), "exit\n")!=0){
 	//Detect pipes
 		k = makeargv(buffer, " \n", &argpath);
+		//Find the pipes, replace them with null characters (to stop execvp).
+		j = 1;
+		queue[0] = 0;
 		for (i=0;i<k;i++){
-			//if (strcmp(argpath[i], "|\n")!=0){
-			
-			//}
-			printf("Command [%i]: %s\n",i,argpath[i]);
+			if (strcmp(argpath[i], "|")==0){
+				argpath[i] = "\0";
+				queue[j] = i+1;
+				j++;
+			}
 		}
 	
-		processid = fork(); //fork the current process (2 identical processes exist now). Let's use them.
-		
+	for(i=0;i<j;i++){
+	printf("Command [%i]: %s\n",i, argpath[queue[i]]);
+	}
+		//processid = fork(); //fork the current process (2 identical processes exist now). Let's use them.
+
+/*
 		switch(processid){
 			case -1:
 				err_sys("Failed to fork");
 				break;
 			case 0: //Child Process
-		        execvp(argpath[0],argpath);write(1,"exec fail\n",10);exit(1);
+			printf("Command [%i]: %s\n",i, argpath[queue[i]]);
+		        execvp(argpath[queue[i]],argpath);write(1,"exec fail\n",10);exit(1);
 				break;
 			default: //Parent Process
 				waitpid(processid, NULL, 0);
 				printf("%s", prompt);
 				break;
-		}
+		}*/
 	}
 	printf("Bye!\n");
 }
